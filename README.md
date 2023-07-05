4808afeab78a0b2aded6644590369c8112ccae19
IonutCiuta8x8 <ionut.ciuta@8x8.com>
2023-07-05 14:46:36 +0300

    Move tasks to README.md

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# swi-social-media-demo
-A demo project for SWI presentations
+## Internship Social Media Demo
+
+## Task for Java in 2023
+### Part 1
+
+**Task1**:
+Can we make the models slimmer? [ records ]
+
+**Task2**:
+Let's display posts, but just the basic stuff [ forEach, pattern matching, enhanced switch, method reference ]
+
+**Task3**:
+Group by author the post count [ var ]
+
+Solution on branch: **java2023-part1**
+
+### Part 2
+**Task1**:
+Let's display posts properly [ refactor Part1.Task2 with streams ]
+
+**Task2**:
+Let's find the latest post for a specific user and print a default message if none found [ Optional ]
+
+**Task3**:
+Let's return a set of all hashtags
+
+**Task4**:
+Let's sort the posts by most liked
+
+**Task5**:
+Let's redo Part1.Task3 and make it prettier
+
+**Task6**:
+What if getting the timeline takes some time and we don't want to block?
+
+Solution on branch: **java2023-part2**
--- src/SocialMediaPlatform.java
+++ src/SocialMediaPlatform.java
@@ -8,28 +8,6 @@ public class SocialMediaPlatform {
         Timeline timeline = timelineService.getTimelineViaApiCall(user);
 
         System.out.println(timeline);
-
-        // Part 1
-        // Task1: can we make the models slimmer? [ records ]
-
-        // Task2: let's display posts, but just the basic stuff [ forEach, pattern matching, enhanced switch, method reference ]
-
-        // Task3: group by author the post count [ var ]
-
-
-        // Part 2
-        // Task1: let's display posts properly [ refactor Part1.Task2 with streams ]
-
-        // Task2: let's find the latest post for a specific user
-        // and print a default message if none found [ Optional ]
-
-        // Task3: let's return a set of all hashtags
-
-        // Task4: let's sort the posts by most liked
-
-        // Task5: let's redo Part1.Task3 and make it prettier
-
-        // Task6: what if getting the timeline takes some time and we don't want to block?
     }
 }
 
